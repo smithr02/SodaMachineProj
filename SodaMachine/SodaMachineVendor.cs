ac40ac7ab52b979a660acdf48531597e32cd71a7
Ryan Smith <ryan_smith2@hotmail.com>
2020-05-05 23:46:10 -0500

    Added a DrinkChoice method, capturing the users choice and removing the corresponding choice from the choices count.

--- SodaMachine/SodaMachineVendor.cs
+++ SodaMachine/SodaMachineVendor.cs
@@ -20,20 +20,34 @@ namespace SodaMachine
         {
             sr = new SodaRegister(10,15,20); // here we can dynamically change the amount of soda cans of each type we want
             cr = new CoinRegister();
+            userCoinPurse = new List<Coin>();
 
         }
 
-        public void Transaction()
+        public void DrinkChoice()
         {
-            userCoinPurse = new List<Coin>();
+            
             Console.WriteLine("What soda do you want?");
             sodaChoice = Console.ReadLine();
-                            
-            for(int i=0; i < usersQuarter; i++)
+
+            if (sodaChoice == "Orange")
             {
-                Quarter q = new Quarter();
-                userCoinPurse.Add(q);
+                sr.orangeCount--;
             }
+            else if (sodaChoice == "Grape")
+            {
+                sr.grapeCount--;
+            }
+            else if (sodaChoice == "Lemon")
+            {
+                sr.lemonCount--;
+            }
+            else
+            {
+                Console.WriteLine("Sorry please pick from Orange, Grape or Lemon.");
+            }
+                            
+            
         }
 
         public void Payment(List<Coin> coins)
@@ -45,23 +59,47 @@ namespace SodaMachine
         {
             Console.WriteLine("How many quarters are you putting in?");
             usersQuarter = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < usersQuarter; i++) //loop will run for however many times the userInputs, in such will add that many objects.
+            {
+                Quarter q = new Quarter();
+                userCoinPurse.Add(q);
+            }
         }
 
         public void PayingInDimes()
         {
             Console.WriteLine("How many dimes are you putting in?");
             usersDime = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < usersDime; i++)
+            {
+                Dime d = new Dime();
+                userCoinPurse.Add(d);
+            }
         }
 
         public void PayingInNickels()
         {
             Console.WriteLine("How many nickels are you putting in?");
             usersNickel = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < usersNickel; i++)
+            {
+                Nickel n = new Nickel();
+                userCoinPurse.Add(n);
+            }
         }
         public void PayInPennies()
         {
             Console.WriteLine("How many pennies are you putting in?");
             usersPenny = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < usersPenny; i++) 
+            {
+                Penny p = new Penny();
+                userCoinPurse.Add(p);
+            }
         }
 
     }
